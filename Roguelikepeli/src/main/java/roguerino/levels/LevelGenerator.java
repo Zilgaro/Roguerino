9516d4dd4acfafcfe9ef3782ba3668817f911f6d
Zilgaro <verner_saaristo@msn.com>
2015-10-17 18:53:45 +0300

    Uusi sekvenssikaavio ja lisäystä javadociin

--- Roguelikepeli/src/main/java/roguerino/levels/LevelGenerator.java
+++ Roguelikepeli/src/main/java/roguerino/levels/LevelGenerator.java
@@ -21,14 +21,28 @@ public class LevelGenerator {
     }
     */
 
+    /**
+     * Tarkempaa katsastusta harrastettuna tämä metodi on melko redundantti; 
+     * levelin konstruktorissa itsessään luodaan jo dimensioiden puitteissa oleva
+     * kartta, jossa on pelkkiä Empty() -blockeja. Kuitenkin LevelGenerator luokkanaan
+     * on oikeutettu: sitä tarvitaan
+     * mikäli lisättäisiin toisenlaisten karttojen generointi, joissa jo
+     * valmiiksi olisi muitakin block -tyyppejä.
+     * @param width annettu leveys
+     * @param height annettu korkeus
+     * @return palauttaa generoidun levelin
+     */
+    
     public Level generateLevelEmpty(int width, int height) { //luo tyhjää(kävelytilaa) täynnä olevan kentän
         Level level = new Level(width, height);
+        /*
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 Blockerino block = new Empty();
                 level.setBlock(i, j, block);
             }
         }
+        */
         return level;
     }
 }
